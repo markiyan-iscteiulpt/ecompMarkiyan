e632ecc47687f1539ad7e45d2e290f3ee8fced3f
Markiyan <whisper199@gmail.com>
2019-09-23 15:58:43 -0700

    Comments register.v

--- l2_verilog/l2_code/register.v
+++ l2_verilog/l2_code/register.v
@@ -1,5 +1,6 @@
 `timescale 1ns/1ps
 
+//Descrição flip-flop tipo D com enable
 module register (
   input		    clk,
   input             en,
@@ -8,8 +9,9 @@ module register (
 );
 
 
+//Processo em que o flip-flop reage ao flanco ascendente de clk
 always @ (posedge clk)
-   if (en == 1'd1) 
+   if (en == 1'd1) //Se en=1, flip-flop copia entrada D para a saída Q 
      data_out <= data_in;
 
 endmodule
